198746ac2dee0ab5069ab4d0bbef801f1e1e5a6e
GustavoCig <gustavo.cignachi@gocase.com.br>
2020-06-03 01:23:33 -0300

    Rellocate database folder, again

---
+++ src/database/connection.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+import knex from 'knex'
+
+dotenv.config()
+
+const connection = knex({
+  client:     'pg',
+  connection: {
+    host:     process.env.DB_HOST,
+    user:     process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
+  }
+})
+
+export default connection
